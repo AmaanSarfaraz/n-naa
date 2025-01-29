1996c39130068921903a233d1f0b8dd213a1edcc
AMAAN SARFARAZ <sarfarazmohammad369@gmail.com>
2025-01-30 00:49:53 +0530

    njbjhb

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@
 
     // CORS configuration
     app.use(cors({
-        origin: ['http://localhost:5173', 'http://localhost:5174', 'https://clever-stardust-0e1a3b.netlify.app'], // Specify allowed origins
+        origin: 'https://clever-stardust-0e1a3b.netlify.app', // Specify allowed origins
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
         allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true // Allow credentials
--- client/src/services/config.js
+++ client/src/services/config.js
@@ -1,2 +1,3 @@
 // export const API_URL = 'https://advanced-hospital-management.vercel.app'
-export const API_URL = 'http://localhost:3000'
+// export const API_URL = 'http://localhost:3000'
+export const API_URL = 'https://n-bqa597t7n-amaan-sarfarazs-projects.vercel.app'
